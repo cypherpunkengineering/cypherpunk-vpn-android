a7041ca5ccb3f30b783b80ca496371e37eeab8e4
teshi04 <teshi04@gmail.com>
2016-06-30 13:29:23 +0900

    Fix background

--- app/src/main/java/com/cypherpunk/android/vpn/ui/LoginActivity.java
+++ app/src/main/java/com/cypherpunk/android/vpn/ui/LoginActivity.java
@@ -5,6 +5,7 @@ import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.view.ViewCompat;
@@ -38,6 +39,10 @@ public class LoginActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
 // Contacts permissions
 //        mayRequestContacts();
 
