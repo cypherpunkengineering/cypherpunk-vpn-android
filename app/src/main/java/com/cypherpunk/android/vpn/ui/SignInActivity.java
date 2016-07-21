9ca21c06240a92841037540ebf97301c06373141
teshi04 <teshi04@gmail.com>
2016-07-21 17:04:06 +0900

    Remove unused request contacts code

--- app/src/main/java/com/cypherpunk/android/vpn/ui/SignInActivity.java
+++ app/src/main/java/com/cypherpunk/android/vpn/ui/SignInActivity.java
@@ -1,23 +1,17 @@
 package com.cypherpunk.android.vpn.ui;
 
-import android.Manifest;
-import android.accounts.Account;
-import android.accounts.AccountManager;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.TaskStackBuilder;
-import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,11 +23,8 @@ import com.cypherpunk.android.vpn.data.api.model.LoginRequest;
 import com.cypherpunk.android.vpn.databinding.ActivitySignInBinding;
 
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
 
 import okhttp3.ResponseBody;
-import pub.devrel.easypermissions.EasyPermissions;
 import retrofit2.adapter.rxjava.HttpException;
 import rx.SingleSubscriber;
 import rx.Subscription;
@@ -42,10 +33,7 @@ import rx.schedulers.Schedulers;
 import rx.subscriptions.Subscriptions;
 
 
-public class SignInActivity extends AppCompatActivity
-        implements EasyPermissions.PermissionCallbacks {
-
-    private static final int REQUEST_GET_ACCOUNTS = 0;
+public class SignInActivity extends AppCompatActivity {
 
     private ActivitySignInBinding binding;
     private ProgressDialog progressDialog;
@@ -60,8 +48,6 @@ public class SignInActivity extends AppCompatActivity
             getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
-// Contacts permissions
-//        mayRequestContacts();
 
         binding.email.requestFocus();
         binding.password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -93,65 +79,6 @@ public class SignInActivity extends AppCompatActivity
     protected void onDestroy() {
         subscription.unsubscribe();
         super.onDestroy();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
-    }
-
-    @Override
-    public void onPermissionsGranted(int requestCode, List<String> perms) {
-        populateAutoComplete();
-    }
-
-    @Override
-    public void onPermissionsDenied(int requestCode, List<String> perms) {
-        // permissions have been denied.
-    }
-
-    private void mayRequestContacts() {
-        if (EasyPermissions.hasPermissions(this, Manifest.permission.GET_ACCOUNTS)) {
-            populateAutoComplete();
-        } else {
-            EasyPermissions.requestPermissions(this, getString(R.string.permission_rationale),
-                    REQUEST_GET_ACCOUNTS, Manifest.permission.GET_ACCOUNTS);
-        }
-    }
-
-    private void populateAutoComplete() {
-        ArrayList<String> mailList = new ArrayList<>();
-        Account[] accounts = AccountManager.get(this).getAccounts();
-        for (Account account : accounts) {
-            String type = account.type;
-            if (type.equals("com.google")) {
-                String mail = account.name;
-                mailList.add(mail);
-            }
-        }
-        addEmailsToAutoComplete(mailList);
-    }
-
-    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(SignInActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-        binding.email.setAdapter(adapter);
-        binding.email.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean hasFocus) {
-                showAccounts();
-            }
-        });
-        showAccounts();
-    }
-
-    private void showAccounts() {
-        if (binding.email.hasFocus() & ViewCompat.isAttachedToWindow(binding.email)) {
-            binding.email.showDropDown();
-        }
     }
 
     private void attemptSignIn() {
