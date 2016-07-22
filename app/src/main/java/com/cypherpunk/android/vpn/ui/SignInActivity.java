e1b92d740c136b7d03ca6844a4183b2dd3b83d10
teshi04 <teshi04@gmail.com>
2016-07-22 17:56:17 +0900

    Fix login screen layout

--- app/src/main/java/com/cypherpunk/android/vpn/ui/SignInActivity.java
+++ app/src/main/java/com/cypherpunk/android/vpn/ui/SignInActivity.java
@@ -3,7 +3,6 @@ package com.cypherpunk.android.vpn.ui;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
@@ -43,11 +42,6 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
 
         binding.email.requestFocus();
         binding.password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
--- app/src/main/java/com/cypherpunk/android/vpn/ui/SignUpActivity.java
+++ app/src/main/java/com/cypherpunk/android/vpn/ui/SignUpActivity.java
@@ -2,7 +2,6 @@ package com.cypherpunk.android.vpn.ui;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.TaskStackBuilder;
@@ -25,11 +24,6 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
 
         binding.email.requestFocus();
         binding.email.setOnEditorActionListener(new TextView.OnEditorActionListener() {
