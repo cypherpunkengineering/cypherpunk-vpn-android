4a6dc92c7812218d6750656dbb705a5c1af11ab2
teshi04 <teshi04@gmail.com>
2016-07-28 12:49:00 +0900

    Fix main screen background

--- app/src/main/java/com/cypherpunk/android/vpn/ui/IntroductionActivity.java
+++ app/src/main/java/com/cypherpunk/android/vpn/ui/IntroductionActivity.java
@@ -5,9 +5,7 @@ import android.databinding.DataBindingUtil;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.cypherpunk.android.vpn.R;
@@ -21,12 +19,13 @@ public class IntroductionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_introduction);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_introduction);
 
         binding.createAccountButton.setOnClickListener(
                 new View.OnClickListener() {
@@ -44,5 +43,4 @@ public class IntroductionActivity extends AppCompatActivity {
                     }
                 });
     }
-
 }
--- app/src/main/java/com/cypherpunk/android/vpn/ui/MainActivity.java
+++ app/src/main/java/com/cypherpunk/android/vpn/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.cypherpunk.android.vpn.ui;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.net.VpnService;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.ActionBar;
@@ -40,6 +41,11 @@ public class MainActivity extends AppCompatActivity implements VpnStatus.StateLi
             builder.addNextIntent(intent);
             builder.startActivities();
             finish();
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
