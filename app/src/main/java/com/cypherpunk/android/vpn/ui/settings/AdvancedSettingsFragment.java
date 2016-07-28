4bd9a2fedf5a75edc7b8566c04596443528045a9
teshi04 <teshi04@gmail.com>
2016-07-28 19:49:29 +0900

    Create setting tabs

---
+++ app/src/main/java/com/cypherpunk/android/vpn/ui/settings/AdvancedSettingsFragment.java
@@ -0,0 +1,15 @@
+package com.cypherpunk.android.vpn.ui.settings;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+import com.cypherpunk.android.vpn.R;
+
+
+public class AdvancedSettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.settings);
+    }
+}
