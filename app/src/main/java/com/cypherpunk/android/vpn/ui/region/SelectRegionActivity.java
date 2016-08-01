e8682fbfd01cf82c35fb7adc493c2d3aed92506f
teshi04 <teshi04@gmail.com>
2016-08-01 17:07:21 +0900

    Create new region screen

--- app/src/main/java/com/cypherpunk/android/vpn/ui/region/SelectRegionActivity.java
+++ app/src/main/java/com/cypherpunk/android/vpn/ui/region/SelectRegionActivity.java
@@ -3,23 +3,28 @@ package com.cypherpunk.android.vpn.ui.region;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.commonsware.cwac.merge.MergeAdapter;
 import com.cypherpunk.android.vpn.R;
 
 
 public class SelectRegionActivity extends AppCompatActivity {
 
     public static String EXTRA_AREA = "area";
-    private static String[] AREA = {"US", "Europe", "Asia", "Australia", "South America", "USA"};
+    private static String[] AREA = {"Auto select region", "North America", "South America", "Europe", "Asia", "Pacific"};
+    private static String[] CONNECTED = {"Tokyo, Japan", "New York, USA"};
     private static final int REQUEST_SELECT_REGION = 1;
+    private ListView listView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,11 +36,29 @@ public class SelectRegionActivity extends AppCompatActivity {
             actionBar.setHomeAsUpIndicator(R.drawable.close_vector);
         }
 
-        final ListView listView = new ListView(this);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_item_area);
-        adapter.addAll(AREA);
-        listView.setAdapter(adapter);
-        listView.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
+        listView = new ListView(this);
+        MergeAdapter mergeAdapter = new MergeAdapter();
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        // recently connected
+        mergeAdapter.addView(buildHeader(R.string.select_region_list_header_recent));
+        ArrayAdapter<String> regionAdapter = new ArrayAdapter<>(this, R.layout.list_item_city);
+        regionAdapter.addAll(CONNECTED);
+        mergeAdapter.addAdapter(regionAdapter);
+        mergeAdapter.addView(inflater.inflate(R.layout.divider, listView, false));
+
+        // region
+        mergeAdapter.addView(buildHeader(R.string.select_region_list_header));
+        ArrayAdapter<String> regionAdapter1 = new ArrayAdapter<>(this, R.layout.list_item_area);
+        regionAdapter1.addAll(AREA);
+        mergeAdapter.addAdapter(regionAdapter1);
+        mergeAdapter.addView(inflater.inflate(R.layout.divider, listView, false));
+
+        // sort by distance
+        mergeAdapter.addView(buildSortByDistanceButton());
+
+        listView.setAdapter(mergeAdapter);
+        listView.setDivider(null);
         setContentView(listView);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -68,5 +91,24 @@ public class SelectRegionActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private View buildHeader(@StringRes int text) {
+        TextView textView
+                = (TextView) LayoutInflater.from(this).inflate(R.layout.list_item_header_region_select, listView, false);
+        textView.setText(text);
+        return textView;
+    }
+
+    private View buildSortByDistanceButton() {
+        View view = LayoutInflater.from(this).inflate(R.layout.list_item_footer_region, listView, false);
+        View button = view.findViewById(R.id.sort_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Sort by distance screen
+            }
+        });
+        return view;
     }
 }
