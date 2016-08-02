a7b4d28a8bd3c329baed5c154d7b6e5c20fdde70
teshi04 <teshi04@gmail.com>
2016-08-02 21:06:38 +0900

    Add switch color

--- app/src/main/java/com/cypherpunk/android/vpn/widget/VpnSwitch.java
+++ app/src/main/java/com/cypherpunk/android/vpn/widget/VpnSwitch.java
@@ -1,9 +1,16 @@
 package com.cypherpunk.android.vpn.widget;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.support.annotation.IntDef;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.widget.SwitchCompat;
 import android.util.AttributeSet;
+
+import com.cypherpunk.android.vpn.R;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -14,6 +21,11 @@ public class VpnSwitch extends SwitchCompat {
     public static final int DISCONNECTED = 0;
     public static final int CONNECTING = 1;
     public static final int CONNECTED = 2;
+
+    private Drawable drawableConnectThumb;
+    private Drawable drawableConnectTrack;
+    private int colorConnectThumb;
+    private int colorConnectTrack;
 
     @Retention(RetentionPolicy.SOURCE)
     @IntDef({DISCONNECTED, CONNECTING, CONNECTED})
@@ -31,6 +43,11 @@ public class VpnSwitch extends SwitchCompat {
     public VpnSwitch(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
+        drawableConnectThumb = ResourcesCompat.getDrawable(getResources(), R.drawable.switch_thumb_solid, null);
+        drawableConnectTrack = ResourcesCompat.getDrawable(getResources(), R.drawable.switch_track_solid, null);
+        colorConnectThumb = ContextCompat.getColor(context, R.color.switch_thumb_connect);
+        colorConnectTrack = ContextCompat.getColor(context, R.color.switch_track_connect);
+
         // エラー回避
         setClickable(true);
         setTextOn("");
@@ -40,13 +57,16 @@ public class VpnSwitch extends SwitchCompat {
     }
 
     public void setStatus(@ConnectionStatus int status) {
-        switch (status) {
-            case DISCONNECTED:
-                break;
-            case CONNECTING:
-                break;
-            case CONNECTED:
-                break;
+        if (status == CONNECTED) {
+            drawableConnectThumb.setColorFilter(colorConnectThumb, PorterDuff.Mode.SRC_ATOP);
+            drawableConnectTrack.setColorFilter(colorConnectTrack, PorterDuff.Mode.SRC_ATOP);
+        } else {
+            drawableConnectThumb.clearColorFilter();
+            drawableConnectTrack.clearColorFilter();
         }
+        LayerDrawable thumbDrawable = (LayerDrawable) getThumbDrawable();
+        LayerDrawable trackDrawable = (LayerDrawable) getTrackDrawable();
+        thumbDrawable.setDrawableByLayerId(R.id.thumb, drawableConnectThumb);
+        trackDrawable.setDrawableByLayerId(R.id.track, drawableConnectTrack);
     }
 }
