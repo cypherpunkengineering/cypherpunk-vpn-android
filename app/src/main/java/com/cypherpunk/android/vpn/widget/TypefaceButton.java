3b477839db0288bd2d9a923dc52633a4d3abc216
teshi04 <teshi04@gmail.com>
2016-08-02 18:18:37 +0900

    Apply button style

---
+++ app/src/main/java/com/cypherpunk/android/vpn/widget/TypefaceButton.java
@@ -0,0 +1,29 @@
+package com.cypherpunk.android.vpn.widget;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatButton;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+import com.cypherpunk.android.vpn.utils.FontUtil;
+
+
+public class TypefaceButton extends Button {
+
+    public TypefaceButton(Context context) {
+        this(context, null);
+    }
+
+    public TypefaceButton(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TypefaceButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        if (!isInEditMode()) {
+            Typeface tf = FontUtil.get(context);
+            setTypeface(tf);
+        }
+    }
+}
