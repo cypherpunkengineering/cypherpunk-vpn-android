04eba35ede857b1b37d2ea74db4f1e0e6d9ed0e0
teshi04 <teshi04@gmail.com>
2016-08-07 16:44:53 +0900

    Create Status screen

---
+++ app/src/main/java/com/cypherpunk/android/vpn/ui/settings/StatusFragment.java
@@ -0,0 +1,29 @@
+package com.cypherpunk.android.vpn.ui.settings;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cypherpunk.android.vpn.R;
+import com.cypherpunk.android.vpn.databinding.FragmentStatusBinding;
+
+
+public class StatusFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_status, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        FragmentStatusBinding binding = FragmentStatusBinding.bind(getView());
+
+    }
+}
