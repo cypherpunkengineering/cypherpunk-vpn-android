67ce56c81317d52d298ca119f03701963b7d1a9c
teshi04 <teshi04@gmail.com>
2016-08-07 16:04:30 +0900

    Create new simple setting screen

--- app/src/main/java/com/cypherpunk/android/vpn/widget/TypefaceRadioButton.java
+++ app/src/main/java/com/cypherpunk/android/vpn/widget/TypefaceRadioButton.java
@@ -4,11 +4,12 @@ import android.content.Context;
 import android.graphics.Typeface;
 import android.support.v7.widget.AppCompatRadioButton;
 import android.util.AttributeSet;
+import android.widget.RadioButton;
 
 import com.cypherpunk.android.vpn.utils.FontUtil;
 
 
-public class TypefaceRadioButton extends AppCompatRadioButton {
+public class TypefaceRadioButton extends RadioButton {
 
     public TypefaceRadioButton(Context context) {
         this(context, null);
