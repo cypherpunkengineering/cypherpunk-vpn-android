fe1a036198c1cad8c3fb4a3990f896f1a1283f4f
teshi04 <teshi04@gmail.com>
2016-08-08 17:55:16 +0900

    Add advanced settings

--- app/src/main/java/com/cypherpunk/android/vpn/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/cypherpunk/android/vpn/ui/settings/SettingsFragment.java
@@ -1,39 +1,15 @@
 package com.cypherpunk.android.vpn.ui.settings;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.RadioGroup;
+import android.support.v7.preference.PreferenceFragmentCompat;
 
 import com.cypherpunk.android.vpn.R;
-import com.cypherpunk.android.vpn.databinding.FragmentSettingsBinding;
 
 
-public class SettingsFragment extends Fragment implements RadioGroup.OnCheckedChangeListener {
-
-    private FragmentSettingsBinding binding;
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
-    }
+public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        binding = FragmentSettingsBinding.bind(getView());
-
-        binding.radioGroup.setOnCheckedChangeListener(this);
-        binding.maxSpeedRadioButton.setChecked(true);
-    }
-
-    @Override
-    public void onCheckedChanged(RadioGroup radioGroup, int id) {
-        // TODO:
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.settings);
     }
 }
