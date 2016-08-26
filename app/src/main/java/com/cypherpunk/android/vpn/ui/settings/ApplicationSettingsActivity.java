8bdef438f136248ff97a3a8d006e935f23980955
teshi04 <teshi04@gmail.com>
2016-08-26 20:56:35 +0900

    Add progress bar

--- app/src/main/java/com/cypherpunk/android/vpn/ui/settings/ApplicationSettingsActivity.java
+++ app/src/main/java/com/cypherpunk/android/vpn/ui/settings/ApplicationSettingsActivity.java
@@ -3,54 +3,52 @@ package com.cypherpunk.android.vpn.ui.settings;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.cypherpunk.android.vpn.R;
 import com.cypherpunk.android.vpn.databinding.ActivityApplicationSettingsBinding;
+import com.cypherpunk.android.vpn.model.AppData;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import rx.subscriptions.Subscriptions;
+
 public class ApplicationSettingsActivity extends AppCompatActivity {
+
+    private Subscription subscription = Subscriptions.empty();
+    private ApplicationAdapter adapter;
+    private ActivityApplicationSettingsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityApplicationSettingsBinding binding
-                = DataBindingUtil.setContentView(this, R.layout.activity_application_settings);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_application_settings);
 
         ActionBar actionBar = getSupportActionBar();
 
+        // TODO: customize search view
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        final PackageManager pm = getPackageManager();
-        List<ApplicationInfo> installedAppList = pm.getInstalledApplications(PackageManager.GET_META_DATA);
-
-        final List<AppData> dataList = new ArrayList<AppData>();
-        for (ApplicationInfo app : installedAppList) {
-            AppData data = new AppData();
-            data.name = app.loadLabel(pm).toString();
-            data.icon = app.loadIcon(pm);
-            dataList.add(data);
-        }
-
+        // TODO: scroll bar
         binding.list.setHasFixedSize(true);
-        ApplicationAdapter adapter = new ApplicationAdapter();
-        adapter.addAll(dataList);
         binding.list.addItemDecoration(new DividerItemDecoration(this));
+        adapter = new ApplicationAdapter();
         binding.list.setAdapter(adapter);
-    }
 
-    public static class AppData {
-        String name;
-        Drawable icon;
+        getApplicationList();
     }
 
     @Override
@@ -67,5 +65,48 @@ public class ApplicationSettingsActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        subscription.unsubscribe();
+        super.onDestroy();
+    }
+
+    private void getApplicationList() {
+        // TODO: sort
+        subscription = Observable
+                .create(new Observable.OnSubscribe<List<AppData>>() {
+                    @Override
+                    public void call(Subscriber<? super List<AppData>> subscriber) {
+                        final PackageManager pm = getPackageManager();
+                        List<ApplicationInfo> installedAppList = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+                        final List<AppData> list = new ArrayList<>();
+                        for (ApplicationInfo app : installedAppList) {
+                            list.add(new AppData(app.loadLabel(pm).toString(), app.loadIcon(pm), app.packageName));
+                        }
+                        subscriber.onNext(list);
+                        subscriber.onCompleted();
+
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<List<AppData>>() {
+                    @Override
+                    public void onCompleted() {
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                    }
+
+                    @Override
+                    public void onNext(List<AppData> list) {
+                        adapter.addAll(list);
+                        binding.progress.setVisibility(View.GONE);
+                        binding.list.setVisibility(View.VISIBLE);
+                    }
+                });
     }
 }
