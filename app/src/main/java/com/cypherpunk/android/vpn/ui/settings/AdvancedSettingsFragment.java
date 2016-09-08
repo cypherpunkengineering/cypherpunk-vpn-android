45669d9b6892d9dd29bbab4a7a7d339aa5670dde
teshi04 <teshi04@gmail.com>
2016-09-08 17:43:59 +0900

    Create local port setting screen

--- app/src/main/java/com/cypherpunk/android/vpn/ui/settings/AdvancedSettingsFragment.java
+++ app/src/main/java/com/cypherpunk/android/vpn/ui/settings/AdvancedSettingsFragment.java
@@ -2,7 +2,6 @@ package com.cypherpunk.android.vpn.ui.settings;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.ArrayRes;
 import android.support.v7.preference.Preference;
@@ -17,8 +16,7 @@ import com.cypherpunk.android.vpn.model.SettingItem;
 
 import java.util.ArrayList;
 
-public class AdvancedSettingsFragment extends PreferenceFragmentCompat
-        implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class AdvancedSettingsFragment extends PreferenceFragmentCompat {
 
     private static final int REQUEST_LIST_SETTING = 1;
 
@@ -97,7 +95,15 @@ public class AdvancedSettingsFragment extends PreferenceFragmentCompat
         });
 
         localPort = findPreference("local_port");
-        localPort.setSummary(cypherpunkSetting.local_port);
+        localPort.setSummary(String.valueOf(cypherpunkSetting.localPort));
+        localPort.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                startActivityForResult(LocalPortActivity.createIntent(getActivity(),
+                        String.valueOf(cypherpunkSetting.localPort)), REQUEST_LIST_SETTING);
+                return true;
+            }
+        });
 
         encryptionLevel = findPreference("encryption_level");
         encryptionLevel.setSummary(cypherpunkSetting.encryptionLevel);
@@ -188,23 +194,13 @@ public class AdvancedSettingsFragment extends PreferenceFragmentCompat
                     cypherpunkSetting.key = data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE);
                     key.setSummary(data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_NAME));
                     break;
+                case "local_port":
+                    cypherpunkSetting.localPort = data.getIntExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE, 8888);
+                    localPort.setSummary(data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_NAME));
+                    break;
             }
             cypherpunkSetting.save();
         }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        getPreferenceScreen().getSharedPreferences()
-                .registerOnSharedPreferenceChangeListener(this);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        getPreferenceScreen().getSharedPreferences()
-                .unregisterOnSharedPreferenceChangeListener(this);
     }
 
     private ArrayList<SettingItem> getSettingItemList(@ArrayRes int valueListRes, @ArrayRes int descriptionListRes) {
@@ -218,13 +214,5 @@ public class AdvancedSettingsFragment extends PreferenceFragmentCompat
             list.add(new SettingItem(valueList[i], descriptionListRes != 0 ? descriptionList[i] : "", valueList[i]));
         }
         return list;
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        cypherpunkSetting = new CypherpunkSetting();
-        if (key.equals("local_port")) {
-            localPort.setSummary(cypherpunkSetting.local_port);
-        }
     }
 }
