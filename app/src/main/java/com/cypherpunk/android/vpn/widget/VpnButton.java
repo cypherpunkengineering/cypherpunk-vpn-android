cdbdccfb26f3f31d35731310413ac4897e1ef732
teshi04 <teshi04@gmail.com>
2016-09-14 18:25:58 +0900

    Create vpn button connecting state

--- app/src/main/java/com/cypherpunk/android/vpn/widget/VpnButton.java
+++ app/src/main/java/com/cypherpunk/android/vpn/widget/VpnButton.java
@@ -6,15 +6,12 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.IntDef;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.content.res.ResourcesCompat;
-import android.support.v4.graphics.drawable.DrawableCompat;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.widget.CompoundButton;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.cypherpunk.android.vpn.R;
 
@@ -30,12 +27,10 @@ public class VpnButton extends FrameLayout {
 
     private CompoundButton.OnCheckedChangeListener listener;
 
+    private ObjectAnimator anim;
     private VpnCoreButton buttonView;
-    private View frameView;
+    private ImageView frameView;
     private Drawable frame;
-    private Drawable drawable;
-    private int connectedFrameColor;
-    private int disconnectedFrameColor;
 
     @Retention(RetentionPolicy.SOURCE)
     @IntDef({DISCONNECTED, CONNECTING, CONNECTED})
@@ -53,55 +48,44 @@ public class VpnButton extends FrameLayout {
     public VpnButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
-        buttonView = new VpnCoreButton(context);
-        frameView = new View(context);
+        frameView = new ImageView(context);
         addView(frameView);
+        buttonView = new VpnCoreButton(context);
         addView(buttonView);
 
-        frame = DrawableCompat.wrap(ContextCompat.getDrawable(context, R.drawable.vpn_button_frame));
-        drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.vpn_button_border_orange, null);
+        frame = ResourcesCompat.getDrawable(getResources(), R.drawable.vpn_button_frame, null);
+        frameView.setImageDrawable(frame);
+
         buttonView.setOnCheckedChangeListener(new VpnCoreButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Log.d("VpnButton", "b ischeck");
                 if (listener != null) {
                     listener.onCheckedChanged(buttonView, isChecked);
                 }
             }
         });
-        disconnectedFrameColor = ContextCompat.getColor(context, R.color.vpn_button_frame_disconnected);
-        connectedFrameColor = ContextCompat.getColor(context, R.color.vpn_button_frame_connected);
+
+        anim = ObjectAnimator.ofFloat(frameView, "rotation", 0f, 360f);
+        anim.setInterpolator(new LinearInterpolator());
+        anim.setDuration(3000);
+        anim.setRepeatCount(ValueAnimator.INFINITE);
 
         setClickable(true);
         setStatus(DISCONNECTED);
     }
 
     public void setStatus(@ConnectionStatus int status) {
-        buttonView.setStatus(status);
         switch (status) {
             case DISCONNECTED:
-                DrawableCompat.setTint(frame, disconnectedFrameColor);
-                frameView.setBackground(frame);
+            case CONNECTED:
+                anim.cancel();
                 break;
             case CONNECTING:
-                frameView.setBackground(drawable);
-                ObjectAnimator anim = ObjectAnimator.ofFloat(frameView, "rotation", 0f, 360f);
-                anim.setInterpolator(new LinearInterpolator());
-                anim.setDuration(3000);
-                anim.setRepeatCount(ValueAnimator.INFINITE);
                 anim.start();
                 break;
-            case CONNECTED:
-                DrawableCompat.setTint(frame, connectedFrameColor);
-                frameView.setBackground(frame);
-
-                break;
         }
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-//            binding.frame.setBackground(frame);
-//        } else {
-//            binding.frame.setBackgroundDrawable(frame);
-//        }
+        buttonView.setStatus(status);
+        frame.setLevel(status);
     }
 
     public void setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener listener) {
@@ -148,6 +132,7 @@ public class VpnButton extends FrameLayout {
 
         public void setStatus(@ConnectionStatus int status) {
             icon.setLevel(status);
+            invalidate();
         }
 
         public void setOnCheckedChangeListener(OnCheckedChangeListener listener) {
