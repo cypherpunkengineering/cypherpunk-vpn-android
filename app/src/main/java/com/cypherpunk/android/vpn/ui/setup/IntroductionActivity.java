63fd94411fabf791ea35a669d9b8896c7422dc7c
teshi04 <teshi04@gmail.com>
2016-09-20 15:14:16 +0900

    Delete unused code

--- app/src/main/java/com/cypherpunk/android/vpn/ui/setup/IntroductionActivity.java
+++ app/src/main/java/com/cypherpunk/android/vpn/ui/setup/IntroductionActivity.java
@@ -24,11 +24,6 @@ public class IntroductionActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-
         ActivityIntroductionBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_introduction);
 
         binding.startFreeButton.setOnClickListener(
