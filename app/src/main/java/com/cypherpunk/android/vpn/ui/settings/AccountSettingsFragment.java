74538fb64b162c8401ac02265344afca73503679
teshi04 <teshi04@gmail.com>
2016-09-28 14:22:19 +0900

    ハードコードしていたところを修正

---
+++ app/src/main/java/com/cypherpunk/android/vpn/ui/settings/AccountSettingsFragment.java
@@ -0,0 +1,98 @@
+package com.cypherpunk.android.vpn.ui.settings;
+
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.View;
+
+import com.cypherpunk.android.vpn.R;
+import com.cypherpunk.android.vpn.data.api.UserManager;
+import com.cypherpunk.android.vpn.ui.setup.IntroductionActivity;
+import com.cypherpunk.android.vpn.vpn.CypherpunkVPN;
+
+
+public class AccountSettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        View rootView = getView();
+        assert rootView != null;
+        setDivider(new ColorDrawable(ContextCompat.getColor(getContext(), R.color.divider)));
+        setDividerHeight(getContext().getResources().getDimensionPixelSize(R.dimen.divider));
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.preference_account);
+
+        // TODO: set username
+        AccountPreference account = (AccountPreference) findPreference("account");
+        account.setUsernameText("wiz");
+
+        // TODO: set plan
+        Preference plan = findPreference("plan");
+        plan.setTitle("Monthly Premium");
+        plan.setSummary("Renews On 02/02/2016");
+
+        // email
+        Preference email = findPreference("email");
+        UserManager userManager = UserManager.getInstance(getContext());
+        email.setSummary(userManager.getMailAddress());
+
+        findPreference("log_out").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                signOut();
+                return true;
+            }
+        });
+
+        findPreference("upgrade").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                startActivity(new Intent(getActivity(), UpgradePlanActivity.class));
+                return true;
+            }
+        });
+
+        findPreference("contact_us").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                startActivity(new Intent(getActivity(), ContactActivity.class));
+                return true;
+            }
+        });
+
+        email.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                startActivity(new Intent(getActivity(), EditEmailActivity.class));
+                return true;
+            }
+        });
+
+        findPreference("password").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                startActivity(new Intent(getActivity(), EditPasswordActivity.class));
+                return true;
+            }
+        });
+    }
+
+    private void signOut() {
+        CypherpunkVPN.stop(getActivity().getApplicationContext(), getActivity().getBaseContext());
+        UserManager manager = UserManager.getInstance(getActivity());
+        manager.clearUser();
+        Intent intent = new Intent(getActivity(), IntroductionActivity.class);
+        TaskStackBuilder builder = TaskStackBuilder.create(getActivity());
+        builder.addNextIntent(intent);
+        builder.startActivities();
+        getActivity().finish();
+    }
+}
--- app/src/main/java/com/cypherpunk/android/vpn/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/cypherpunk/android/vpn/ui/settings/SettingsFragment.java
@@ -1,21 +1,35 @@
 package com.cypherpunk.android.vpn.ui.settings;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.support.v4.app.TaskStackBuilder;
+import android.support.annotation.ArrayRes;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceManager;
 import android.view.View;
 
 import com.cypherpunk.android.vpn.R;
-import com.cypherpunk.android.vpn.data.api.UserManager;
-import com.cypherpunk.android.vpn.ui.setup.IntroductionActivity;
-import com.cypherpunk.android.vpn.vpn.CypherpunkVPN;
+import com.cypherpunk.android.vpn.model.CypherpunkSetting;
+import com.cypherpunk.android.vpn.model.SettingItem;
 
+import java.util.ArrayList;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
+
+    private static final int REQUEST_LIST_SETTING = 1;
+
+    private CypherpunkSetting cypherpunkSetting;
+    private Preference protocol;
+    private Preference remotePort;
+    private Preference localPort;
+    private Preference firewall;
+    private EncryptionPreference encryptionLevel;
+    private Preference cipher;
+    private Preference authentication;
+    private Preference key;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -28,71 +42,150 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        PreferenceManager preferenceManager = getPreferenceManager();
+        preferenceManager.setSharedPreferencesName("cypherpunk_setting");
         addPreferencesFromResource(R.xml.preference_settings);
 
-        // TODO: set username
-        AccountPreference account = (AccountPreference) findPreference("account");
-        account.setUsernameText("wiz");
+        cypherpunkSetting = new CypherpunkSetting();
 
-        // TODO: set plan
-        Preference plan = findPreference("plan");
-        plan.setTitle("Monthly Premium");
-        plan.setSummary("Renews On 02/02/2016");
-
-        // email
-        Preference email = findPreference("email");
-        UserManager userManager = UserManager.getInstance(getContext());
-        email.setSummary(userManager.getMailAddress());
-
-        findPreference("log_out").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+        findPreference("application").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                signOut();
+                startActivity(new Intent(getActivity(), ApplicationSettingsActivity.class));
                 return true;
             }
         });
 
-        findPreference("upgrade").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+        findPreference("trusted_network").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                startActivity(new Intent(getActivity(), UpgradePlanActivity.class));
+                startActivity(new Intent(getActivity(), NetworkActivity.class));
                 return true;
             }
         });
 
-        findPreference("contact_us").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+        protocol = findPreference("protocol");
+        protocol.setSummary(cypherpunkSetting.protocol);
+        protocol.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                startActivity(new Intent(getActivity(), ContactActivity.class));
+                startActivityForResult(ListPreferenceActivity.createIntent(getActivity(),
+                        "protocol", protocol.getTitle(), new CypherpunkSetting().protocol, getSettingItemList(
+                                R.array.protocol_value, R.array.protocol_description)),
+                        REQUEST_LIST_SETTING);
                 return true;
             }
         });
 
-        email.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+        remotePort = findPreference("remote_port");
+        remotePort.setSummary(cypherpunkSetting.remotePort);
+        remotePort.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                startActivity(new Intent(getActivity(), EditEmailActivity.class));
+                startActivityForResult(ListPreferenceActivity.createIntent(getActivity(),
+                        "remote_port", remotePort.getTitle(), new CypherpunkSetting().remotePort,
+                        getSettingItemList(R.array.remote_port_value, 0)),
+                        REQUEST_LIST_SETTING);
                 return true;
             }
         });
 
-        findPreference("password").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+        firewall = findPreference("firewall");
+        firewall.setSummary(cypherpunkSetting.firewall);
+        firewall.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                startActivity(new Intent(getActivity(), EditPasswordActivity.class));
+                startActivityForResult(ListPreferenceActivity.createIntent(getActivity(),
+                        "firewall", firewall.getTitle(), new CypherpunkSetting().firewall,
+                        getSettingItemList(R.array.firewall_value, R.array.firewall_description)),
+                        REQUEST_LIST_SETTING);
+                return true;
+            }
+        });
+
+        localPort = findPreference("local_port");
+        localPort.setSummary(String.valueOf(cypherpunkSetting.localPort));
+        localPort.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                startActivityForResult(LocalPortActivity.createIntent(getActivity(),
+                        String.valueOf(cypherpunkSetting.localPort)), REQUEST_LIST_SETTING);
+                return true;
+            }
+        });
+
+        encryptionLevel = (EncryptionPreference) findPreference("encryption_level");
+        encryptionLevel.setSummary(cypherpunkSetting.encryptionLevel);
+        encryptionLevel.setCipherText(cypherpunkSetting.cipher);
+        encryptionLevel.setAuthText(cypherpunkSetting.authentication);
+        encryptionLevel.setKeyText(cypherpunkSetting.key);
+        encryptionLevel.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                startActivityForResult(ListPreferenceActivity.createIntent(getActivity(),
+                        "encryption_level", encryptionLevel.getTitle(),
+                        new CypherpunkSetting().encryptionLevel, getSettingItemList(
+                                R.array.encryption_value, R.array.encryption_description)),
+                        REQUEST_LIST_SETTING);
                 return true;
             }
         });
     }
 
-    private void signOut() {
-        CypherpunkVPN.stop(getActivity().getApplicationContext(), getActivity().getBaseContext());
-        UserManager manager = UserManager.getInstance(getActivity());
-        manager.clearUser();
-        Intent intent = new Intent(getActivity(), IntroductionActivity.class);
-        TaskStackBuilder builder = TaskStackBuilder.create(getActivity());
-        builder.addNextIntent(intent);
-        builder.startActivities();
-        getActivity().finish();
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            String updateKey = data.getStringExtra(ListPreferenceActivity.EXTRA_KEY);
+            CypherpunkSetting cypherpunkSetting = new CypherpunkSetting();
+            switch (updateKey) {
+                case "protocol":
+                    cypherpunkSetting.protocol = data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE);
+                    protocol.setSummary(data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE));
+                    break;
+                case "remote_port":
+                    cypherpunkSetting.remotePort = data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE);
+                    remotePort.setSummary(data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE));
+                    break;
+                case "firewall":
+                    cypherpunkSetting.firewall = data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE);
+                    firewall.setSummary(data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE));
+                    break;
+                case "encryption_level":
+                    cypherpunkSetting.encryptionLevel = data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE);
+                    encryptionLevel.setSummary(data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE));
+                    break;
+//                case "cipher":
+//                    cypherpunkSetting.cipher = data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE);
+//                    cipher.setSummary(data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE));
+//                    break;
+//                case "authentication":
+//                    cypherpunkSetting.authentication = data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE);
+//                    authentication.setSummary(data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE));
+//                    break;
+//                case "key":
+//                    cypherpunkSetting.key = data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE);
+//                    key.setSummary(data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE));
+//                    break;
+                case "local_port":
+                    cypherpunkSetting.localPort = data.getIntExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE, 8888);
+                    localPort.setSummary(data.getStringExtra(ListPreferenceActivity.EXTRA_SELECTED_VALUE));
+                    break;
+            }
+            cypherpunkSetting.save();
+        }
+    }
+
+    private ArrayList<SettingItem> getSettingItemList(@ArrayRes int valueListRes, @ArrayRes int descriptionListRes) {
+        String[] valueList = getResources().getStringArray(valueListRes);
+        String[] descriptionList = new String[0];
+        if (descriptionListRes != 0) {
+            descriptionList = getResources().getStringArray(descriptionListRes);
+        }
+        ArrayList<SettingItem> list = new ArrayList<>();
+        for (int i = 0; i < valueList.length; i++) {
+            list.add(new SettingItem(valueList[i], descriptionListRes != 0 ? descriptionList[i] : ""));
+        }
+        return list;
     }
 }
