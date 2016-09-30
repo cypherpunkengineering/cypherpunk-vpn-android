adaeee2af1659ce23ad751a39506439a534214a3
J. Maurice <j@wiz.biz>
2016-09-30 14:38:49 +0900

    Add OpenVPN 3.1 support

--- openvpn/main/jni/Application.mk
+++ openvpn/main/jni/Application.mk
@@ -1,7 +1,7 @@
 APP_ABI := arm64-v8a armeabi armeabi-v7a mips  x86 x86_64
 APP_PLATFORM := android-14
 
-APP_STL:=stlport_static
+APP_STL:=gnustl_static
 #APP_STL:=gnustl_shared
 
 
@@ -10,5 +10,5 @@ APP_STL:=stlport_static
 #LOCAL_ARM_MODE := arm
 
 #NDK_TOOLCHAIN_VERSION=clang
-APP_CPPFLAGS += -std=c++1y
+APP_CPPFLAGS += -std=c++11
 APP_CFLAGS += -funwind-tables
