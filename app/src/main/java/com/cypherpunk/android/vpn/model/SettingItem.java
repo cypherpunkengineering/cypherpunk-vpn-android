e4d9e4bff97402182e50f0b741b1addd325db8af
J. Maurice <j@wiz.biz>
2016-10-02 11:17:54 +0900

    Integrate settings, move strings into strings file

--- app/src/main/java/com/cypherpunk/android/vpn/model/SettingItem.java
+++ app/src/main/java/com/cypherpunk/android/vpn/model/SettingItem.java
@@ -4,10 +4,12 @@ import java.io.Serializable;
 
 public class SettingItem implements Serializable {
 
+    public final String key;
     public final String value;
     public final String description;
 
-    public SettingItem(String value, String description) {
+    public SettingItem(String key, String value, String description) {
+        this.key = key;
         this.value = value;
         this.description = description;
     }
