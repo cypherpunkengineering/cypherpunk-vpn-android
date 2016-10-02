a08638719194f3efc036442200c631ac3910fb95
J. Maurice <j@wiz.biz>
2016-10-03 06:50:58 +0900

    Disable RateDialog from appearing after every disconnect for now

--- app/src/main/java/com/cypherpunk/android/vpn/ui/main/MainActivity.java
+++ app/src/main/java/com/cypherpunk/android/vpn/ui/main/MainActivity.java
@@ -116,8 +116,10 @@ public class MainActivity extends AppCompatActivity
                 }
                 if (status.isConnected()) {
                     stopVpn();
+                    /*
                     RateDialogFragment dialogFragment = RateDialogFragment.newInstance();
                     dialogFragment.show(getSupportFragmentManager());
+                    */
                 }
                 if (!status.isConnected() && !status.isDisconnected()) {
                     // connecting
