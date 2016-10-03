32168f91f2b69baa48f422ac37ecb18ecf80e834
teshi04 <teshi04@gmail.com>
2016-10-03 19:45:15 +0900

    Create new Region model

--- app/src/main/java/com/cypherpunk/android/vpn/model/Region.java
+++ app/src/main/java/com/cypherpunk/android/vpn/model/Region.java
@@ -21,58 +21,87 @@ public class Region implements RealmModel {
     private String id;
 
     @Required
-    private String area;
+    private String city;
     @Required
     private String countryCode;
     @Required
-    private String city;
-    @Required
     private String ipAddress;
+    @Required
+    private String nationalFlagUrl;
+    private boolean favorited;
+    private boolean selected;
+    private int mapX;
+    private int mapY;
 
     @SuppressWarnings("unused")
     public Region() {
     }
 
-    public Region(@NonNull String area,
-                  @NonNull String countryCode,
-                  @NonNull String city,
-                  @NonNull String ipAddress) {
-        this.area = area;
-        this.countryCode = countryCode;
+    public Region(
+            @NonNull String city,
+            @NonNull String countryCode,
+            @NonNull String ipAddress,
+            @NonNull String nationalFlagUrl,
+            int mapX,
+            int mapY) {
         this.city = city;
+        this.countryCode = countryCode;
         this.ipAddress = ipAddress;
+        this.nationalFlagUrl = nationalFlagUrl;
+        this.mapX = mapX;
+        this.mapY = mapY;
 
-        id = calcId(countryCode, area, city);
+        id = calcId(countryCode, city);
     }
 
     public String getId() {
         return id;
     }
 
-    public String getArea() {
-        return area;
+    public String getCity() {
+        return city;
     }
 
     public String getCountryCode() {
         return countryCode;
     }
 
-    public String getCity() {
-        return city;
-    }
-
     public String getIpAddress() {
         return ipAddress;
+    }
+
+    public String getNationalFlagUrl() {
+        return nationalFlagUrl;
+    }
+
+    public boolean isFavorited() {
+        return favorited;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public int getMapX() {
+        return mapX;
+    }
+
+    public int getMapY() {
+        return mapY;
     }
 
     @Override
     public String toString() {
         return "Region{" +
                 "id='" + id + '\'' +
-                ", area='" + area + '\'' +
-                ", countryCode='" + countryCode + '\'' +
                 ", city='" + city + '\'' +
+                ", countryCode='" + countryCode + '\'' +
                 ", ipAddress='" + ipAddress + '\'' +
+                ", nationalFlagUrl='" + nationalFlagUrl + '\'' +
+                ", favorited=" + favorited +
+                ", selected=" + selected +
+                ", mapX=" + mapX +
+                ", mapY=" + mapY +
                 '}';
     }
 
@@ -90,8 +119,8 @@ public class Region implements RealmModel {
         return id != null ? id.hashCode() : 0;
     }
 
-    private static String calcId(String countryCode, String area, String city) {
-        final String str = area + "##" + countryCode + "##" + city;
+    private static String calcId(String countryCode, String city) {
+        final String str = countryCode + "##" + city;
 
         try {
             final MessageDigest md = MessageDigest.getInstance("SHA-256");
