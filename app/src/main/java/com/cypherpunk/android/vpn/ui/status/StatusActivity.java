a38efee28fa6a1f8f61ada3b1178d059ca5e7cb0
teshi04 <teshi04@gmail.com>
2016-10-06 13:59:11 +0900

    Fix status activity

--- app/src/main/java/com/cypherpunk/android/vpn/ui/status/StatusActivity.java
+++ app/src/main/java/com/cypherpunk/android/vpn/ui/status/StatusActivity.java
@@ -21,6 +21,7 @@ import com.cypherpunk.android.vpn.databinding.ActivityStatusBinding;
 import com.cypherpunk.android.vpn.model.IpStatus;
 import com.cypherpunk.android.vpn.model.Location;
 import com.cypherpunk.android.vpn.vpn.CypherpunkVpnStatus;
+import com.squareup.picasso.Picasso;
 
 import javax.inject.Inject;
 
@@ -141,6 +142,7 @@ public class StatusActivity extends AppCompatActivity implements VpnStatus.State
                 if (connected) {
                     binding.map.setNewPosition(location.getMapX(), location.getMapY());
                     binding.newLocation.setText(location.getCity());
+                    Picasso.with(StatusActivity.this).load(location.getNationalFlagUrl()).into(binding.newNationalFlag);
                 }
                 binding.map.setNewPositionVisibility(connected);
             }
