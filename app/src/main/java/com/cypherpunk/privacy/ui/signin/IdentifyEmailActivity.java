6bb3141a0180c684913002550bba20a941360bc8
Yuki Anzai <anzai.y.aa@gmail.com>
2017-05-04 18:32:58 +0900

    change databinding to butterknife on IdentifyEmailActivity

--- app/src/main/java/com/cypherpunk/privacy/ui/signin/IdentifyEmailActivity.java
+++ app/src/main/java/com/cypherpunk/privacy/ui/signin/IdentifyEmailActivity.java
@@ -3,32 +3,32 @@ package com.cypherpunk.privacy.ui.signin;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
 import android.text.TextUtils;
-import android.text.TextWatcher;
 import android.util.Patterns;
-import android.view.KeyEvent;
-import android.view.View;
 import android.view.inputmethod.EditorInfo;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.cypherpunk.privacy.CypherpunkApplication;
 import com.cypherpunk.privacy.R;
 import com.cypherpunk.privacy.data.api.CypherpunkService;
 import com.cypherpunk.privacy.data.api.json.EmailRequest;
-import com.cypherpunk.privacy.databinding.ActivityIdentifyEmailBinding;
 import com.cypherpunk.privacy.ui.common.FullScreenProgressDialog;
 
 import java.net.UnknownHostException;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnEditorAction;
+import butterknife.OnTextChanged;
 import okhttp3.ResponseBody;
 import retrofit2.adapter.rxjava.HttpException;
 import rx.SingleSubscriber;
@@ -44,56 +44,49 @@ public class IdentifyEmailActivity extends AppCompatActivity {
         return new Intent(context, IdentifyEmailActivity.class);
     }
 
-    private ActivityIdentifyEmailBinding binding;
-    @Nullable
-    private FullScreenProgressDialog dialog;
     @NonNull
     private Subscription subscription = Subscriptions.empty();
+
+    @Nullable
+    private FullScreenProgressDialog dialog;
 
     @Inject
     CypherpunkService webService;
 
+    @BindView(R.id.text_input_layout)
+    TextInputLayout textInputLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_identify_email);
+        setContentView(R.layout.activity_identify_email);
+        ButterKnife.bind(this);
+
         ((CypherpunkApplication) getApplication()).getAppComponent().inject(this);
 
         if (!getResources().getBoolean(R.bool.is_tablet)) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         }
+    }
 
-        binding.editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
+    @OnTextChanged(R.id.edit_text)
+    void onTextChanged() {
+        textInputLayout.setError(null);
+        textInputLayout.setErrorEnabled(false);
+    }
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                binding.textInputLayout.setError(null);
-                binding.textInputLayout.setErrorEnabled(false);
-            }
+    @OnEditorAction(R.id.edit_text)
+    boolean onEditorAction(int id) {
+        if (id == EditorInfo.IME_ACTION_DONE) {
+            identifyEmail();
+            return true;
+        }
+        return false;
+    }
 
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-        });
-        binding.editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == EditorInfo.IME_ACTION_DONE) {
-                    identifyEmail();
-                    return true;
-                }
-                return false;
-            }
-        });
-        binding.submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                identifyEmail();
-            }
-        });
+    @OnClick(R.id.submit_button)
+    void onSubmitButtonClicked() {
+        identifyEmail();
     }
 
     @Override
@@ -107,17 +100,19 @@ public class IdentifyEmailActivity extends AppCompatActivity {
     }
 
     private void identifyEmail() {
-        final String email = binding.editText.getText().toString();
+        final EditText editText = textInputLayout.getEditText();
+        assert editText != null;
+        final String email = editText.getText().toString();
 
         if (TextUtils.isEmpty(email)) {
-            binding.textInputLayout.setError(getString(R.string.error_field_required));
-            binding.editText.requestFocus();
+            textInputLayout.setError(getString(R.string.error_field_required));
+            editText.requestFocus();
             return;
         }
 
         if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            binding.textInputLayout.setError(getString(R.string.error_invalid_email));
-            binding.editText.requestFocus();
+            textInputLayout.setError(getString(R.string.error_invalid_email));
+            editText.requestFocus();
             return;
         }
 
