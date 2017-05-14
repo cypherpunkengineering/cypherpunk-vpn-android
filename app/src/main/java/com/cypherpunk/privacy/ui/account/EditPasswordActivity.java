8bd2683b592a40424d8251310013f6e0fa179fd9
Yuki Anzai <anzai.y.aa@gmail.com>
2017-05-14 19:11:57 +0900

    EditPasswordのUIを変更

--- app/src/main/java/com/cypherpunk/privacy/ui/account/EditPasswordActivity.java
+++ app/src/main/java/com/cypherpunk/privacy/ui/account/EditPasswordActivity.java
@@ -1,27 +1,34 @@
 package com.cypherpunk.privacy.ui.account;
 
+import android.content.Context;
 import android.content.pm.ActivityInfo;
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.cypherpunk.privacy.CypherpunkApplication;
 import com.cypherpunk.privacy.R;
 import com.cypherpunk.privacy.data.api.CypherpunkService;
 import com.cypherpunk.privacy.data.api.json.ChangePasswordRequest;
-import com.cypherpunk.privacy.databinding.ActivityEditPasswordBinding;
-import com.cypherpunk.privacy.ui.signin.ProgressFragment;
+import com.cypherpunk.privacy.ui.common.FullScreenProgressDialog;
 
 import java.net.UnknownHostException;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnTextChanged;
 import okhttp3.ResponseBody;
 import rx.SingleSubscriber;
 import rx.Subscription;
@@ -31,34 +38,63 @@ import rx.subscriptions.Subscriptions;
 
 public class EditPasswordActivity extends AppCompatActivity {
 
-    private ActivityEditPasswordBinding binding;
-    private ProgressFragment dialogFragment = ProgressFragment.newInstance();
+    @NonNull
     private Subscription subscription = Subscriptions.empty();
+
+    @Nullable
+    private FullScreenProgressDialog dialog;
 
     @Inject
     CypherpunkService webService;
 
+    @BindView(R.id.text_input_layout_password)
+    TextInputLayout passwordTextInputLayout;
+
+    @BindView(R.id.text_input_layout_confirm_password)
+    TextInputLayout confirmPasswordTextInputLayout;
+
+    @BindView(R.id.text_input_layout_current_password)
+    TextInputLayout currentPasswordTextInputLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_password);
+        ButterKnife.bind(this);
+
         ((CypherpunkApplication) getApplication()).getAppComponent().inject(this);
 
         if (!getResources().getBoolean(R.bool.is_tablet)) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         }
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_password);
+        final Toolbar toolbar = ButterKnife.findById(this, R.id.toolbar);
 
-        setSupportActionBar(binding.toolbar.toolbar);
-        ActionBar actionBar = getSupportActionBar();
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setDisplayShowTitleEnabled(false);
-            binding.toolbar.title.setText(R.string.title_activity_edit_password);
             actionBar.setHomeAsUpIndicator(R.drawable.close_vector);
         }
+    }
 
-        binding.password.requestFocus();
+    @OnTextChanged(R.id.password)
+    void onPasswordTextChanged() {
+        passwordTextInputLayout.setError(null);
+        passwordTextInputLayout.setErrorEnabled(false);
+    }
+
+    @OnTextChanged(R.id.confirm_password)
+    void onConfirmPasswordTextChanged() {
+        confirmPasswordTextInputLayout.setError(null);
+        confirmPasswordTextInputLayout.setErrorEnabled(false);
+    }
+
+    @OnTextChanged(R.id.current_password)
+    void onCurrentPasswordTextChanged() {
+        currentPasswordTextInputLayout.setError(null);
+        currentPasswordTextInputLayout.setErrorEnabled(false);
     }
 
     @Override
@@ -73,11 +109,9 @@ public class EditPasswordActivity extends AppCompatActivity {
             case android.R.id.home:
                 finish();
                 return true;
+
             case R.id.action_done:
-                if (validatePassword()) {
-                    updatePassword(binding.currentPassword.getText().toString(),
-                            binding.password.getText().toString());
-                }
+                updatePassword();
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -86,63 +120,86 @@ public class EditPasswordActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         subscription.unsubscribe();
+        if (dialog != null) {
+            dialog.dismiss();
+            dialog = null;
+        }
         super.onDestroy();
     }
 
-    private boolean validatePassword() {
-        binding.password.setError(null);
-        binding.confirmEmail.setError(null);
-        binding.currentPassword.setError(null);
+    private void updatePassword() {
+        final EditText passwordEditText = passwordTextInputLayout.getEditText();
+        assert passwordEditText != null;
+        final String password = passwordEditText.getText().toString();
 
-        String newPassword = binding.password.getText().toString();
-        String confirmPassword = binding.confirmEmail.getText().toString();
-        String currentPassword = binding.currentPassword.getText().toString();
+        final EditText confirmPasswordEditText = confirmPasswordTextInputLayout.getEditText();
+        assert confirmPasswordEditText != null;
+        final String confirmPassword = confirmPasswordEditText.getText().toString();
 
-        boolean result = true;
-        if (TextUtils.isEmpty(newPassword)) {
-            binding.password.setError(getString(R.string.error_field_required));
-            result = false;
+        final EditText currentPasswordEditText = currentPasswordTextInputLayout.getEditText();
+        assert currentPasswordEditText != null;
+        final String currentPassword = currentPasswordEditText.getText().toString();
+
+        View focusView = null;
+
+        if (TextUtils.isEmpty(password)) {
+            passwordTextInputLayout.setError(getString(R.string.error_field_required));
+            focusView = passwordEditText;
         }
 
         if (TextUtils.isEmpty(confirmPassword)) {
-            binding.confirmEmail.setError(getString(R.string.error_field_required));
-            result = false;
+            confirmPasswordTextInputLayout.setError(getString(R.string.error_field_required));
+            if (focusView == null) {
+                focusView = confirmPasswordEditText;
+            }
+        } else if (!TextUtils.equals(password, confirmPassword)) {
+            confirmPasswordTextInputLayout.setError(getString(R.string.edit_password_account_error_do_not_match));
+            if (focusView == null) {
+                focusView = confirmPasswordEditText;
+            }
         }
 
         if (TextUtils.isEmpty(currentPassword)) {
-            binding.currentPassword.setError(getString(R.string.error_field_required));
-            result = false;
+            currentPasswordTextInputLayout.setError(getString(R.string.error_field_required));
+            if (focusView == null) {
+                focusView = currentPasswordEditText;
+            }
         }
 
-        if (!newPassword.equals(confirmPassword)) {
-            binding.confirmEmail.setError(getString(R.string.edit_password_account_error_do_not_match));
-            result = false;
+        if (focusView != null) {
+            focusView.requestFocus();
+            return;
         }
 
-        return result;
-    }
+        dialog = new FullScreenProgressDialog(this);
+        dialog.show();
 
-    private void updatePassword(@NonNull final String oldPassword, @NonNull final String newPassword) {
-        dialogFragment.show(getSupportFragmentManager());
+        final Context context = this;
 
         subscription = webService
-                .changePassword(new ChangePasswordRequest(oldPassword, newPassword))
+                .changePassword(new ChangePasswordRequest(currentPassword, password))
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new SingleSubscriber<ResponseBody>() {
                     @Override
                     public void onSuccess(ResponseBody result) {
-                        dialogFragment.dismiss();
+                        if (dialog != null) {
+                            dialog.dismiss();
+                            dialog = null;
+                        }
                         finish();
                     }
 
                     @Override
                     public void onError(Throwable error) {
-                        dialogFragment.dismiss();
+                        if (dialog != null) {
+                            dialog.dismiss();
+                            dialog = null;
+                        }
                         if (error instanceof UnknownHostException) {
-                            Toast.makeText(EditPasswordActivity.this, R.string.no_internet, Toast.LENGTH_SHORT).show();
+                            Toast.makeText(context, R.string.no_internet, Toast.LENGTH_SHORT).show();
                         } else {
-                            Toast.makeText(EditPasswordActivity.this, R.string.api_error, Toast.LENGTH_SHORT).show();
+                            Toast.makeText(context, R.string.api_error, Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
