1b95b9e6b8fde61ef82b2f9d26ce613ebdc73c58
J. Maurice <j@wiz.biz>
2017-06-12 14:05:42 +0900

    Update "Contact Us" and "Help Center" URLs

--- app/src/main/java/com/cypherpunk/privacy/ui/common/Urls.java
+++ app/src/main/java/com/cypherpunk/privacy/ui/common/Urls.java
@@ -5,8 +5,8 @@ package com.cypherpunk.privacy.ui.common;
  */
 public class Urls {
     public static final String STORE = "https://play.google.com/store/apps/details?id=com.cypherpunk.privacy";
-    public static final String CONTACT_US = "https://cypherpunk.zendesk.com/hc/requests/new";
-    public static final String HELP = "https://support.cypherpunk.com/hc";
+    public static final String CONTACT_US = "https://cypherpunk.com/support/request/new";
+    public static final String HELP = "https://cypherpunk.com/support";
     public static final String TERMS_OF_SERVICE = "https://cypherpunk.com/terms-of-service";
     public static final String PRIVACY_POLICY = "https://cypherpunk.com/privacy-policy";
 }
