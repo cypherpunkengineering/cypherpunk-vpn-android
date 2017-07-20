f23663660dbffc4aec5d211742e76a49dec269c5
Yuki Anzai <anzai.y.aa@gmail.com>
2017-07-20 10:25:38 +0900

    fix quick tile

--- app/src/main/java/com/cypherpunk/privacy/ui/vpn/CypherpunkTileService.java
+++ app/src/main/java/com/cypherpunk/privacy/ui/vpn/CypherpunkTileService.java
@@ -1,18 +1,20 @@
 package com.cypherpunk.privacy.ui.vpn;
 
 import android.annotation.TargetApi;
+import android.content.res.Resources;
 import android.os.Build;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import android.support.annotation.NonNull;
 
 import com.cypherpunk.privacy.CypherpunkApplication;
+import com.cypherpunk.privacy.R;
+import com.cypherpunk.privacy.domain.model.AccountSetting;
 import com.cypherpunk.privacy.vpn.VpnStatusHolder;
 
 import javax.inject.Inject;
 
 import de.blinkt.openvpn.core.VpnStatus;
-import timber.log.Timber;
 
 import static com.cypherpunk.privacy.ui.vpn.VpnConnectionIntentService.Mode.TILE;
 
@@ -21,6 +23,9 @@ public class CypherpunkTileService extends TileService implements VpnStatusHolde
 
     @Inject
     VpnStatusHolder vpnStatusHolder;
+
+    @Inject
+    AccountSetting accountSetting;
 
     @Override
     public void onCreate() {
@@ -31,6 +36,7 @@ public class CypherpunkTileService extends TileService implements VpnStatusHolde
     @Override
     public void onStartListening() {
         super.onStartListening();
+        updateTile(vpnStatusHolder.status());
         vpnStatusHolder.addListener(this);
     }
 
@@ -43,22 +49,48 @@ public class CypherpunkTileService extends TileService implements VpnStatusHolde
     @Override
     public void onClick() {
         super.onClick();
-        Timber.d("onClick()");
         startService(VpnConnectionIntentService.createIntent(this, TILE));
+    }
+
+    private void updateTile(@NonNull VpnStatus.ConnectionStatus status) {
+        final Resources res = getApplicationContext().getResources();
+
+        final Tile t = getQsTile();
+
+        if (!accountSetting.isSignedIn() || accountSetting.isPending() || !accountSetting.isActive()) {
+            t.setState(Tile.STATE_UNAVAILABLE);
+            t.setLabel(res.getString(R.string.app_name));
+            t.updateTile();
+            return;
+        }
+
+        switch (status) {
+            case LEVEL_CONNECTED:
+                t.setLabel(res.getString(R.string.status_connected));
+                break;
+
+            case LEVEL_START:
+            case LEVEL_CONNECTING_NO_SERVER_REPLY_YET:
+            case LEVEL_CONNECTING_SERVER_REPLIED:
+                t.setState(Tile.STATE_ACTIVE);
+                t.setLabel(res.getString(R.string.status_connecting));
+                break;
+
+            case LEVEL_NOT_CONNECTED:
+                t.setState(Tile.STATE_INACTIVE);
+                t.setLabel(res.getString(R.string.status_disconnected));
+                break;
+
+            default:
+                t.setState(Tile.STATE_UNAVAILABLE);
+                t.setLabel(res.getString(R.string.app_name));
+                break;
+        }
+        t.updateTile();
     }
 
     @Override
     public void onStateChanged(@NonNull VpnStatus.ConnectionStatus status) {
-        final Tile t = getQsTile();
-        switch (status) {
-            case LEVEL_AUTH_FAILED:
-            case LEVEL_NOT_CONNECTED:
-                t.setState(Tile.STATE_INACTIVE);
-                break;
-            default:
-                t.setState(Tile.STATE_ACTIVE);
-                break;
-        }
-        t.updateTile();
+        updateTile(status);
     }
 }
