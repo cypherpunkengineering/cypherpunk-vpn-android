e8989235206c5c10c9ba7b0b8f0eefef5bf2baeb
Yuki Anzai <anzai.y.aa@gmail.com>
2017-07-23 10:50:37 +0900

    change api endpoint for resendEmail

--- domain/src/main/java/com/cypherpunk/privacy/domain/repository/retrofit/CypherpunkService.java
+++ domain/src/main/java/com/cypherpunk/privacy/domain/repository/retrofit/CypherpunkService.java
@@ -32,7 +32,7 @@ public interface CypherpunkService {
     @POST("/api/v1/account/register/signup")
     Single<StatusResult> signUp(@Body @NonNull SignUpRequest request);
 
-    @POST("/api/v1/account/recover/email")
+    @POST("/api/v1/account/confirm/resend")
     Completable resendEmail(@Body @NonNull EmailRequest request);
 
     @POST("/api/v1/account/authenticate/userpasswd")
