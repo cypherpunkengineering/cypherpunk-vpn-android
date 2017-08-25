5480f81deee1d885d6e47e3d71572e479d1e30ae
Kim Nilsson <kim@cypherpunk.com>
2017-08-25 20:24:48 +0900

    Handle multiple IPs returned from location API (fixes #486)
    Conflicts:
    	app/src/main/java/com/cypherpunk/privacy/vpn/ServerPingerThinger.java
    	app/src/main/java/com/cypherpunk/privacy/vpn/VpnManager.java

--- app/src/main/java/com/cypherpunk/privacy/vpn/VpnManager.java
+++ app/src/main/java/com/cypherpunk/privacy/vpn/VpnManager.java
@@ -174,19 +174,19 @@ public class VpnManager {
         final TunnelMode tunnelMode = vpnSetting.tunnelMode();
         switch (tunnelMode) {
             case MAX_SPEED:
-                list.add("remote " + vpnServer.ovNone() + " " + removePort);
+                list.add("remote " + vpnServer.ovNone().split(",", -1)[0] + " " + removePort);
                 list.add("cipher " + tunnelMode.cipher().value());
                 break;
             case RECOMMENDED:
-                list.add("remote " + vpnServer.ovDefault() + " " + removePort);
+                list.add("remote " + vpnServer.ovDefault().split(",", -1)[0] + " " + removePort);
                 list.add("cipher " + tunnelMode.cipher().value());
                 break;
             case MAX_PRIVACY:
-                list.add("remote " + vpnServer.ovStrong() + " " + removePort);
+                list.add("remote " + vpnServer.ovStrong().split(",", -1)[0] + " " + removePort);
                 list.add("cipher " + tunnelMode.cipher().value());
                 break;
             case MAX_STEALTH:
-                list.add("remote " + vpnServer.ovStealth() + " " + removePort);
+                list.add("remote " + vpnServer.ovStealth().split(",", -1)[0] + " " + removePort);
                 list.add("cipher " + tunnelMode.cipher().value());
                 list.add("scramble obfuscate cypherpunk-xor-key"); // requires xorpatch'd openvpn
                 break;
